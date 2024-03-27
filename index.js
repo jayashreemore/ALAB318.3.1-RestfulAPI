2272b5746b3be6094d6a9d517d98c3822ad29032
Jayshree Morey <120532991+jayashreemore@users.noreply.github.com>
2024-03-27 17:38:17 -0400

    Part 1 of ALAB done

--- index.js
+++ index.js
@@ -29,26 +29,26 @@ ${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
 });
 
 // Valid API Keys.
-apiKeys = ["perscholas", "ps-example", "hJAsknw-L198sAJD-l3kasx"];
+//apiKeys = ["perscholas", "ps-example", "hJAsknw-L198sAJD-l3kasx"];
 
 // New middleware to check for API keys!
 // Note that if the key is not verified,
 // we do not call next(); this is the end.
 // This is why we attached the /api/ prefix
 // to our routing at the beginning!
-app.use("/api", function (req, res, next) {
-  var key = req.query["api-key"];
+// app.use("/api", function (req, res, next) {
+//   var key = req.query["api-key"];
 
-  // Check for the absence of a key.
-  if (!key) next(error(400, "API Key Required"));
+//   // Check for the absence of a key.
+//   if (!key) next(error(400, "API Key Required"));
 
-  // Check for key validity.
-  if (apiKeys.indexOf(key) === -1) next(error(401, "Invalid API Key"));
+//   // Check for key validity.
+//   if (apiKeys.indexOf(key) === -1) next(error(401, "Invalid API Key"));
 
-  // Valid key! Store it in req.key for route access.
-  req.key = key;
-  next();
-});
+//   // Valid key! Store it in req.key for route access.
+//   req.key = key;
+//   next();
+// });
 
 // Use our Routes
 app.use("/api/users", users);
